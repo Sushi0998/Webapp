d68744144f599845416712eb0ba11c6619cf176a
Suraj Shinde <99321988+Sushi0998@users.noreply.github.com>
2024-09-14 17:23:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ const URLS_TO_CACHE = [
   'https://script.google.com/macros/s/AKfycbyPEsGBejD4YkZBue-YbWl5fCY6MbOes3DlYmp9LoYMZz5AfDEd2ByQdfFvD4-bwpCt9A/exec',
   'https://raw.githubusercontent.com/Sushi0998/Webapp/main/manifest.json',
   'https://raw.githubusercontent.com/Sushi0998/Webapp/main/service-worker.js',
-  ''
+  'https://raw.githubusercontent.com/Sushi0998/Webapp/main/Icon.jpg'
   
 ];
 
